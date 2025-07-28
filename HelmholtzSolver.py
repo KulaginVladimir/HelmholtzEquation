cd98f1dd35afe9f2b1eeac750760757e31a2dc7c
KulaginVladimir <kulagin.vladimir.l@yandex.ru>
2025-07-28 07:04:20 +0300

    added solver and examples

--- HelmholtzSolver.py
+++ HelmholtzSolver.py
@@ -1,116 +1,117 @@
 import numpy as np
-import cmath
 
+class HelmholtzSolver:
+    def __init__(self, lambda0: float, boundaries: list, epsilons: list, theta: float=0, polarization: str="s"):
+        """
+        Args:
+            lambda0 (float): wavelength in vacuum
+            boundaries (list): list of structure layers
+            epsilons (list): list determining the piecewise dielectric permittivity
+            theta (float, optional): angle of incidence, deg. Defaults to 0.
+            polarization (str, optional): wave polarizations. Defaults to "s".
+        """
 
-def calculate_RTA(lambda0, boundaries, epsilons, theta=0, polarization="s"):
-    """
-    Вычисляет амплитудные коэффициенты отражения (R), пропускания (T) и поглощения (A)
-    для слоистой структуры с произвольным профилем диэлектрической проницаемости.
+        self.lambda0 = lambda0
+        self.k0 = 2 * np.pi / lambda0
+        self.boundaries = boundaries
+        self.thicknesses = np.diff(boundaries)
+        self.epsilons = epsilons
+        self.theta = np.deg2rad(theta)
 
-    Параметры:
-        lambda0      : Длина волны в вакууме
-        boundaries   : Массив границ слоев [z0, z1, z2, ..., zN]
-        epsilons     : Массив диэлектрических проницаемостей для каждого слоя
-        theta        : Угол падения в градусах (по умолчанию 0)
-        polarization : Поляризация ('s' или 'p', по умолчанию 's')
+        if polarization.lower() not in ["s", "p"]:
+            raise ValueError("Polarization should be 's' or 'p'")
+        self.polarization = polarization.lower()
 
-    Возвращает:
-        R, T (комплексные амплитуды), A (действительное поглощение)
-    """
-    # Проверка входных данных
-    n_layers = len(epsilons)
-    if len(boundaries) != n_layers + 1:
-        raise ValueError("Количество границ должно быть len(epsilons) + 1")
+        if len(self.thicknesses) != len(epsilons):
+            raise ValueError("Number of layers must equal to the number of provided permittivities")
+    
+    def interface_matrix(self, gamma_left: complex, gamma_right: complex):
+        """Computes the interface matrix
 
-    # Константы и параметры
-    k0 = 2 * np.pi / lambda0
-    theta_rad = np.deg2rad(theta)
-    kx = k0 * np.sin(theta_rad)
-    z1 = boundaries[0]
-    zN = boundaries[-1]
+        Args:
+            gamma_left (complex): k (or k/epsilon) at the left, m^-1
+            gamma_right (complex): k (or k/epsilon) at the right, m^-1
 
-    # Функция для вычисления kz с правильным выбором ветви
-    def calc_kz(eps):
-        expr = k0**2 * eps - kx**2
-        return np.sqrt(expr)
+        Returns:
+            array: 2x2 interface matrix
+        """
 
-    # Волновые числа в вакууме
-    eps_vacuum = 1.0
-    kz0 = calc_kz(eps_vacuum)
+        r = (gamma_left - gamma_right) / (gamma_left + gamma_right)
+        t = 2 * gamma_left / (gamma_left + gamma_right)
+        I = np.array([[1, r], [r, 1]], dtype=complex) / t        
 
-    # Вычисление kz для каждого слоя с правильным выбором ветви
-    kz_list = [calc_kz(eps) for eps in epsilons]
+        return I
 
-    # Инициализация матрицы переноса
-    M = np.eye(2, dtype=complex)
+    def propagation_matrix(self, kz: complex, d: float):
+        """Computes the propagation matrix
 
-    # Первый интерфейс: вакуум -> первый слой
-    if polarization == "s":
-        # s-поляризация: используем kz
-        k_left, k_right = kz0, kz_list[0]
-    else:  # p-поляризация
-        # p-поляризация: используем p = kz/eps
-        k_left = kz0 / eps_vacuum
-        k_right = kz_list[0] / epsilons[0]
+        Args:
+            kz (complex): z-component of the wave vector, m^-1
+            d (float): layer thickness, m
 
-    r = (k_left - k_right) / (k_left + k_right)
-    t = 2 * k_left / (k_left + k_right)
-    I = np.array([[1, r], [r, 1]], dtype=complex) / t
-    M = M @ I
+        Returns:
+            array: 2x2 propagation matrix
+        """
 
-    # Обработка каждого слоя
-    for i in range(n_layers):
-        # Матрица распространения
-        d = boundaries[i + 1] - boundaries[i]
-        kz = kz_list[i]
         P = np.array(
-            [[np.exp(-1j * kz * d), 0], [0, np.exp(1j * kz * d)]], dtype=complex
+            [
+                [np.exp(-1j * kz * d), 0], 
+                [0, np.exp(1j * kz * d)]
+            ], 
+                dtype=complex
         )
-        M = M @ P
 
-        # Следующий интерфейс
-        if i < n_layers - 1:
-            # Интерфейс между слоями
-            kz_left, kz_right = kz_list[i], kz_list[i + 1]
-            eps_left, eps_right = epsilons[i], epsilons[i + 1]
+        return P
+    
+    def compute_kz(self, epsilon: complex):
+        """Computes z-component of the wave vector
+
+        Args:
+            epsilon (complex): dielectric permittivity
+
+        Returns:
+            complex: z-component of the wave vector, m^-1
+        """
+
+        kx = self.k0 * np.sin(self.theta)
+        kz = np.sqrt(self.k0**2 * epsilon - kx**2 + 0j)
+
+        if np.imag(kz) < 0:
+            kz = -kz
+        return kz
+    
+    def compute_coefficients(self):
+
+        epsilons_full = [1] + self.epsilons + [1]
+        kz_list = [self.compute_kz(eps) for eps in epsilons_full]
+
+        if self.polarization == "p":
+            gamma_list = [kz/eps for kz, eps in zip(kz_list, epsilons_full)]
         else:
-            # Последний интерфейс: слой -> вакуум
-            kz_left, kz_right = kz_list[i], kz0
-            eps_left, eps_right = epsilons[i], eps_vacuum
+            gamma_list = kz_list
 
-        if polarization == "s":
-            k_left, k_right = kz_left, kz_right
-        else:  # p-поляризация
-            k_left = kz_left / eps_left
-            k_right = kz_right / eps_right
+        M = np.eye(2, dtype=complex)
 
-        r = (k_left - k_right) / (k_left + k_right)
-        t = 2 * k_left / (k_left + k_right)
-        I = np.array([[1, r], [r, 1]], dtype=complex) / t
-        M = M @ I
+        for i, d in enumerate(self.thicknesses):
+            I = self.interface_matrix(gamma_list[i], gamma_list[i+1])
+            M = M @ I
 
-    # Вычисление коэффициентов
-    T = np.exp(1j * kz0 * (zN - z1)) / M[0, 0]
+            P = self.propagation_matrix(kz_list[i+1], d)
+            M = M @ P
 
-    if polarization == "s":
-        R = (M[1, 0] / M[0, 0]) * np.exp(2j * kz0 * z1)
-    else:  # p-поляризация
-        R = -(M[1, 0] / M[0, 0]) * np.exp(2j * kz0 * z1)
+        I_last = self.interface_matrix(gamma_list[-2], gamma_list[-1])
+        M = M @ I_last
 
-    # Поглощение
-    R2 = np.abs(R) ** 2
-    T2 = np.abs(T) ** 2
-    A = 1 - R2 - T2
+        total_length = self.boundaries[-1] - self.boundaries[0]
+        T = np.exp(-1j * kz_list[0] * total_length) / M[0, 0]
 
-    return R, T, A
+        R = (M[1, 0] / M[0, 0]) * np.exp(2j * kz_list[0] * self.boundaries[0])
 
+        if self.polarization == "p":
+            R = -R
 
-# Параметры структуры
-R, T, A = calculate_RTA(
-    lambda0=0.5,
-    boundaries=[0, 1],
-    epsilons=[-2.25 + 1j],  # стекло
-    theta=45,
-    polarization="s",
-)
-print(np.abs(R) ** 2, np.abs(T) ** 2, A)
+        R2 = np.abs(R) ** 2
+        T2 = np.abs(T) ** 2
+        A = 1 - R2 - T2
+
+        return R, T, A
